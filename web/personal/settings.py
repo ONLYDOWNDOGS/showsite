f76b5d9a45ea3d5fd7c7e5a4a27fc7e951c1c76b
Jonathan Cooke <joncooke90@protonmail.com>
2021-06-27 16:33:53 -0600

    Fixed requirements.txt for modern packages, fixed headers on webpages

--- web/personal/settings.py
+++ web/personal/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = ['protected-depths-05682.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'sendemail.apps.SendemailConfig',
-    'bootstrap4',
     'appone.apps.ApponeConfig',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
